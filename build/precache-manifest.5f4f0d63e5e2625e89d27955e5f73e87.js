fa10edf9e35465390470669aaed45b0b26dadc73
Yash Sharma <yashsharma1859@gmail.com>
2020-06-13 23:50:51 +0530

    Second commit

---
+++ build/precache-manifest.5f4f0d63e5e2625e89d27955e5f73e87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a693f23d98e414e1b7b8af551adc2a24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdac484c6676a43e301d",
+    "url": "/static/js/2.2b74ab78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b74ab78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf58259c0c5fa39cfdc",
+    "url": "/static/js/main.3faf29e6.chunk.js"
+  },
+  {
+    "revision": "d97f914eb77fec37191a",
+    "url": "/static/js/runtime-main.e3443e94.js"
+  }
+]);
